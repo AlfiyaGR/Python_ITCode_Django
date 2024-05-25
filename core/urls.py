01547060f6b437ff23e03e013ae728d86ae8f27a
Alfa <alfa@example.com>
2024-05-25 23:01:51 +0300

    Student pages

--- core/urls.py
+++ core/urls.py
@@ -4,29 +4,32 @@ from core import views
 
 
 urlpatterns = [
+    # главная страница (вход учителя и ученика)
     path('', views.home, name='home'),
-    path('homeStud/', views.homeStud, name='homeStud'),
-
-    path('schools/', views.SchoolList.as_view(), name='schools'),
-    path('teachers/', views.TeacherList.as_view(), name='teachers'),
-    path('teacher/<int:pk>/', views.TeacherDetail.as_view(), name='teacher'),
-    path('student/<int:pk>/', views.StudentDetail.as_view(), name='student'),
-
-    path('groups/', views.GroupList.as_view(), name='groups'),
-    path('group/<int:pk>/', views.GroupDetail.as_view(), name='group'),
-
-    path('schedule/<int:pk>/', views.ScheduleList.as_view(), name='schedule'),
-    path('school/<int:pk>/', views.SchoolDetail.as_view(), name='school'),
-    path('lessons/<int:pk>/', views.LessonDetail.as_view(), name='lessons'),
-    path('homeworks/<int:pk>/', views.HomeworkList.as_view(), name='homeworks'),
-
+    path('home_stud/', views.home_stud, name='home_stud'),
     path('get_student', views.get_student, name='get_student'),
     path('get_teacher', views.get_teacher, name='get_teacher'),
 
+    # списки
+    path('schools/', views.SchoolList.as_view(), name='schools'),
+    path('teachers/', views.TeacherList.as_view(), name='teachers'),
+    path('groups/', views.GroupList.as_view(), name='groups'),
+
+    # личный кабинет учителя
+    path('teacher/<int:pk>/', views.TeacherDetail.as_view(), name='teacher'),
+    path('schedule/<int:pk>/', views.ScheduleList.as_view(), name='schedule'),
+    path('school/<int:pk>/', views.SchoolDetail.as_view(), name='school'),
+    path('group/<int:pk>/', views.GroupDetail.as_view(), name='group'),
+
+    # личный кабинет ученика
+    path('student/<int:pk>/', views.StudentDetail.as_view(), name='student'),
+    path('stud_schedule/<int:pk>/', views.StudScheduleList.as_view(), name='stud_schedule'),
+    path('stud_group/<int:pk>/', views.StudGroupDetail.as_view(), name='stud_group'),
+    path('stud_school/<int:pk>/', views.StudSchoolDetail.as_view(), name='stud_school'),
+
+    # все остальное
+    path('lessons/<int:pk>/', views.LessonDetail.as_view(), name='lessons'),
+    path('homeworks/<int:pk>/', views.HomeworkList.as_view(), name='homeworks'),
+
     path('redirect/', views.RedirectAdmin.as_view(), name='redirect'),
 ]
-'''
-urlpatterns += [
-    url(r'^teacher/(?P<pk>[-\w]+)/$', views.get_teacher, name='get_teacher'),
-]
-'''
--- core/views.py
+++ core/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
-
 from django.urls import reverse
 from django.views.generic import TemplateView, ListView, DetailView, RedirectView, FormView
-
 from core import models, forms
 
-'''
-def index(request):
-    context = {
-        'title': 'Главная'
-    }
-    return render(request, 'core/index.html', context)
-'''
+""" Авторизация и главная страница """
 
 
+# Форма авторизации учителя
 def get_teacher(request):
     login = request.GET['login']
     password = request.GET['password']
@@ -26,6 +19,7 @@ def get_teacher(request):
     return render(request, 'core/teacher.html', {'teacher': teacher, 'groups': groups})
 
 
+# Форма авторизации ученика
 def get_student(request):
     login = request.GET['login']
     password = request.GET['password']
@@ -35,42 +29,20 @@ def get_student(request):
     return render(request, 'core/student.html', {'student': student, })
 
 
+# Главная страница (вход учителя)
 def home(request):
     return render(request, 'core/index.html')
 
 
-def homeStud(request):
-    return render(request, 'core/indexStud.html')
+# Главная страница (вход ученика)
+def home_stud(request):
+    return render(request, 'core/index_stud.html')
 
 
-'''
-def get_all_schools(request):
-    schools = models.School.objects.all()
-    context = {
-        'schools': schools,
-        'title': 'Список классов'
-    }
-    return render(request, 'core/schools.html', context)
-
-'''
+""" Личный кабинет учителя """
 
 
-class RedirectAdmin(RedirectView):
-    query_string = True
-    url = '/admin/'
-
-
-class RedirectAuthorization(RedirectView):
-    query_string = True
-    url = '/accounts/login/'
-
-
-class SchoolList(ListView):
-    model = models.School
-    context_object_name = "schools"
-    template_name = 'core/schools.html'
-
-
+# Расписание учителя
 class ScheduleList(ListView):
     model = models.Teacher
     template_name = 'core/schedule.html'
@@ -89,38 +61,166 @@ class ScheduleList(ListView):
         return context
 
 
-'''
-def get_all_teachers(request):
-    teachers = models.Teacher.objects.all()
-    context = {
-        'teachers': teachers,
-        'title': 'Список классов'
-    }
-    return render(request, 'core/teachers.html', context)
+# Список студентов классов учителя
+class GroupDetail(DetailView):
+    model = models.Group
+    template_name = 'core/group.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        teacher = models.Teacher.objects.get(group=self.kwargs['pk'])
+        students = models.Student.objects.filter(group=self.kwargs['pk'])
+        groups = models.Group.objects.filter(teacher=teacher.pk)
+
+        context['students'] = students
+        context['groups'] = groups
+        context['teacher'] = teacher
+        return context
 
 
-def get_all_groups(request):
-    groups = models.Group.objects.all()
-    context = {
-        'groups': groups,
-        'title': 'Список классов'
-    }
-    return render(request, 'core/groups.html', context)
-'''
+# Подробности об учителе
+class TeacherDetail(DetailView):
+    model = models.Teacher
+    template_name = 'core/teacher.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        teacher = models.Teacher.objects.get(pk=self.kwargs['pk'])
+        groups = models.Group.objects.filter(teacher=self.kwargs['pk'])
+
+        context['teacher'] = teacher
+        context['groups'] = groups
+        return context
 
 
+# Подробности о школе для учителя
+class SchoolDetail(DetailView):
+    model = models.Teacher
+    template_name = 'core/school.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        teacher = models.Teacher.objects.get(pk=self.kwargs['pk'])
+        school = models.School.objects.get(pk=teacher.school.pk)
+        groups = models.Group.objects.filter(teacher=teacher.pk)
+
+        context['school'] = school
+        context['teacher'] = teacher
+        context['groups'] = groups
+        return context
+
+
+""" Личный кабинет ученика """
+
+
+# Подробности о студенте
+class StudentDetail(DetailView):
+    model = models.Student
+    template_name = 'core/student.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        student = models.Student.objects.get(pk=self.kwargs['pk'])
+
+        context['student'] = student
+        return context
+
+
+# Список учеников класса ученика
+class StudGroupDetail(DetailView):
+    model = models.Student
+    template_name = 'core/stud_group.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        student = models.Student.objects.get(pk=self.kwargs['pk'])
+        group = models.Group.objects.get(pk=student.group.pk)
+        teacher = models.Teacher.objects.get(group=group.pk)
+        students = models.Student.objects.filter(group=group.pk)
+
+        context['students'] = students
+        context['student'] = student
+        context['teacher'] = teacher
+        return context
+
+
+# Подробности о школе для ученика
+class StudSchoolDetail(DetailView):
+    model = models.Student
+    template_name = 'core/school.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        student = models.Student.objects.get(pk=self.kwargs['pk'])
+        school = models.School.objects.get(pk=student.group.school.pk)
+
+        context['school'] = school
+        context['student'] = student
+        return context
+
+
+""" Все остальное """
+
+
+# Перенаправление на админскую панель
+class RedirectAdmin(RedirectView):
+    query_string = True
+    url = '/admin/'
+
+
+# Перенаправление на страницу автооризации (User)
+class RedirectAuthorization(RedirectView):
+    query_string = True
+    url = '/accounts/login/'
+
+
+# Список всех школ
+class SchoolList(ListView):
+    model = models.School
+    context_object_name = "schools"
+    template_name = 'core/schools.html'
+
+
+# Расписание ученика
+class StudScheduleList(ListView):
+    model = models.Student
+    template_name = 'core/schedule.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        student = models.Student.objects.get(pk=self.kwargs['pk'])
+        group = models.Group.objects.get(pk=student.group.pk)
+        schedules = models.Schedule.objects.filter(group=group.pk)
+
+        context['student'] = student
+        context['schedules'] = schedules
+        context['group'] = group
+
+        return context
+
+
+# Список всех учителей
 class TeacherList(ListView):
     model = models.Teacher
     context_object_name = "teachers"
     template_name = 'core/teachers.html'
 
 
+# Список всех классов
 class GroupList(ListView):
     model = models.Group
     context_object_name = "groups"
     template_name = 'core/groups.html'
 
 
+# Подробности урока
 class LessonDetail(DetailView):
     model = models.Lesson
     template_name = 'core/lessons.html'
@@ -139,6 +239,7 @@ class LessonDetail(DetailView):
         return context
 
 
+# Список домашних работ по уроку
 class HomeworkList(ListView):
     model = models.Lesson
     template_name = 'core/homeworks.html'
@@ -160,6 +261,40 @@ class HomeworkList(ListView):
 
 
 '''
+def index(request):
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, 'core/index.html', context)
+
+
+def get_all_schools(request):
+    schools = models.School.objects.all()
+    context = {
+        'schools': schools,
+        'title': 'Список классов'
+    }
+    return render(request, 'core/schools.html', context)
+
+
+def get_all_teachers(request):
+    teachers = models.Teacher.objects.all()
+    context = {
+        'teachers': teachers,
+        'title': 'Список классов'
+    }
+    return render(request, 'core/teachers.html', context)
+
+
+def get_all_groups(request):
+    groups = models.Group.objects.all()
+    context = {
+        'groups': groups,
+        'title': 'Список классов'
+    }
+    return render(request, 'core/groups.html', context)
+    
+    
 def get_group(request, group_id):
     students = models.Student.objects.filter(group=group_id)
     groups = models.Group.objects.all()
@@ -172,65 +307,3 @@ def get_group(request, group_id):
     }
     return render(request, 'core/group.html', context)
 '''
-
-
-class GroupDetail(DetailView):
-    model = models.Group
-    template_name = 'core/group.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        teacher = models.Teacher.objects.get(group=self.kwargs['pk'])
-        students = models.Student.objects.filter(group=self.kwargs['pk'])
-        groups = models.Group.objects.filter(teacher=teacher.pk)
-
-        context['students'] = students
-        context['groups'] = groups
-        context['teacher'] = teacher
-        return context
-
-
-class TeacherDetail(DetailView):
-    model = models.Teacher
-    template_name = 'core/teacher.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        teacher = models.Teacher.objects.get(pk=self.kwargs['pk'])
-        groups = models.Group.objects.filter(teacher=self.kwargs['pk'])
-
-        context['teacher'] = teacher
-        context['groups'] = groups
-        return context
-
-
-class StudentDetail(DetailView):
-    model = models.Student
-    template_name = 'core/student.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        student = models.Student.objects.get(pk=self.kwargs['pk'])
-
-        context['student'] = student
-        return context
-
-
-class SchoolDetail(DetailView):
-    model = models.Teacher
-    template_name = 'core/school.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        teacher = models.Teacher.objects.get(pk=self.kwargs['pk'])
-        school = models.School.objects.get(pk=teacher.school.pk)
-        groups = models.Group.objects.filter(teacher=teacher.pk)
-
-        context['school'] = school
-        context['teacher'] = teacher
-        context['groups'] = groups
-        return context
